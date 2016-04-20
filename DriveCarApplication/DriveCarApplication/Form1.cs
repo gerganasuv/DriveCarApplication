0cd53f4d683a35cf51649e70cdee80bf5c58caed
Your Name <you@example.com>
2016-04-20 14:22:06 +0300

    Drive_Car_Project

---
+++ DriveCarApplication/DriveCarApplication/Form1.cs
@@ -0,0 +1,332 @@
+﻿namespace DriveCarApplication
+{
+    using System;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        private int anotherCar2X;
+
+        private int anotherCar2Y;
+
+        private int anotherCar2Ypossition;
+
+        private int anotherCarX;
+
+        private int anotherCarY;
+
+        // позицията на първата кола
+        private int carX;
+
+        private int carY;
+
+        private int coinX;
+
+        private int coinY;
+
+        bool left;
+
+        private int level = 50;
+
+        private int levelValue;
+
+        // животи и точки
+        private int lives = 3;
+
+        private int points = 0;
+
+        private Random random; // кара черната кола да пада хаотично,т.е. не само в левия край на таблото
+
+        bool right;
+
+        private int speed = 15;
+
+        public Form1()
+        {
+            this.InitializeComponent();
+            this.InitializeGame();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                // проверка за натиснат бутон
+                // и променя стойността на координатите 
+                this.right = true;
+                this.carX += 60;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                this.left = true;
+                this.carX -= 60;
+            }
+
+            if (this.carX < 50)
+            {
+                this.carX = 50;
+            }
+
+            // проверка на животите и пускане на играта отново със спейс
+            if (this.timer1.Enabled == false)
+            {
+                if (this.lives > 0)
+                {
+                    if (e.KeyCode == Keys.Enter)
+                    {
+                        this.timer1.Enabled = true;
+                        this.anotherCarY = 50;
+                        this.anotherCar2Y = this.anotherCar2Ypossition;
+                        if (this.lives > 0)
+                        {
+                            this.gameover.Text = string.Empty;
+                        }
+                    }
+                }
+                else
+                {
+                    if (e.KeyCode == Keys.Space)
+                    {
+                        this.timer1.Enabled = false;
+                    }
+                }
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                this.right = false;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                this.left = false;
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void InitializeGame()
+        {
+            // въвежда размерите на колните,редовете и т.н.
+            this.carX = 50; // стартова позицияX на колата
+            this.carY = 350; // стартова позицияY на колата
+            this.anotherCar2X = 110;
+            this.anotherCar2Ypossition = 150;
+            this.anotherCar2Y = this.anotherCar2Ypossition;
+            this.anotherCarX = 110;
+            this.anotherCarY = 50;
+            this.coinX = 170;
+            this.coinY = 100;
+
+            this.random = new Random(31);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void screen_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.left)
+            {
+                this.player.Location = new Point(this.carX, this.carY);
+            }
+ // задава конкретна позиция на колата
+            if (this.right)
+            {
+                this.player.Location = new Point(this.carX, this.carY);
+            }
+
+            // увеличава скоростта според точките
+            if (this.points == this.level)
+            {
+                this.level += 50;
+                this.speed += 1;
+            }
+
+            // задава първа кола на позиция...
+            this.anotherCar.Location = new Point(this.anotherCarX, this.anotherCarY);
+            this.anotherCarY += this.speed;
+
+            // задава втора кола на позиция...
+            this.anotherCar2.Location = new Point(this.anotherCar2X, this.anotherCar2Y);
+            this.anotherCar2Y += this.speed;
+
+            // монета
+            this.Coin.Location = new Point(this.coinX, this.coinY);
+            this.coinY += this.speed;
+
+            // при контакт с първата кола спира играта
+            if (this.anotherCarY + 70 >= this.carY && this.anotherCarX == this.carX)
+            {
+                this.timer1.Enabled = false;
+                this.lives--;
+                this.label1.Text = this.lives.ToString();
+                if (this.lives > 0)
+                {
+                    this.gameover.Text = "Press space\n to continue.";
+                }
+            }
+
+            if (this.coinY >= this.carY && this.coinX == this.carX)
+            {
+                this.lives++;
+                this.label1.Text = this.lives.ToString();
+                this.coinY = 0;
+            }
+
+            this.anotherCar2Ypossition = this.random.Next(0, 30); // избира рандом позиция
+
+            if (this.anotherCar2Ypossition >= 0 && this.anotherCar2Ypossition <= 10)
+            {
+                this.anotherCar2Ypossition = 50;
+            }
+            else if (this.anotherCar2Ypossition >= 11 && this.anotherCar2Ypossition <= 20)
+            {
+                this.anotherCar2Ypossition = 130;
+            }
+            else
+            {
+                this.anotherCar2Ypossition = -30;
+            }
+
+            // при контакт с втората кола спира играта
+            if (this.anotherCar2Y + 70 >= this.carY && this.anotherCar2X == this.carX)
+            {
+                this.timer1.Enabled = false;
+                this.lives--;
+                this.label1.Text = this.lives.ToString();
+                if (this.lives > 0)
+                {
+                    this.gameover.Text = "Press space\n to continue.";
+                }
+            }
+
+            if (this.anotherCar2Ypossition == -30)
+            {
+                if (this.anotherCar2Y - 20 >= this.carY && this.anotherCar2X == this.carX)
+                {
+                    this.timer1.Enabled = false;
+                    this.lives--;
+                    this.label1.Text = this.lives.ToString();
+                }
+            }
+
+            if (this.anotherCarY >= 390)
+            {
+                // когато врората кола стигне/подмине дъното( 350 )
+                // я пуска отново на първа линия и random позиция
+                this.anotherCar2Y = this.anotherCar2Ypossition;
+                this.anotherCarY = 50;
+
+                this.anotherCarX = this.random.Next(0, 30);
+                {
+                    if (this.anotherCarX < 10)
+                    {
+                        this.anotherCarX = 50;
+                    }
+                    else if (this.anotherCarX >= 10 && this.anotherCarX < 20)
+                    {
+                        this.anotherCarX = 110;
+                    }
+                    else
+                    {
+                        this.anotherCarX = 170;
+                    }
+
+                    this.anotherCar2X = this.random.Next(0, 40);
+                    if (this.anotherCarX == 50)
+                    {
+                        if (this.anotherCar2X >= 10 && this.anotherCar2X < 20)
+                        {
+                            this.anotherCar2X = 110;
+                        }
+                        else
+                        {
+                            this.anotherCar2X = 170;
+                        }
+                    }
+
+                    if (this.anotherCarX == 110)
+                    {
+                        if (this.anotherCar2X < 10)
+                        {
+                            this.anotherCar2X = 50;
+                        }
+                        else
+                        {
+                            this.anotherCar2X = 170;
+                        }
+                    }
+
+                    if (this.anotherCarX == 170)
+                    {
+                        if (this.anotherCar2X < 10)
+                        {
+                            this.anotherCar2X = 50;
+                        }
+                        else
+                        {
+                            this.anotherCar2X = 110;
+                        }
+                    }
+
+                    if (this.anotherCar2X > 30 && this.anotherCar2X <= 40)
+                    {
+                        this.anotherCar2X = 5000;
+                    }
+                }
+
+                if (this.coinY >= 390)
+                {
+                    this.coinY = 50;
+
+                    this.coinY = this.random.Next(0, 40);
+                    {
+                        if (this.coinX < 10)
+                        {
+                            this.coinX = 170;
+                        }
+                        else if (this.coinX >= 10 && this.coinX < 20)
+                        {
+                            this.coinX = 50;
+                        }
+                        else if (this.coinX >= 21 && this.coinX < 30)
+                        {
+                            this.coinX = 110;
+                        }
+                        else if (this.coinX >= 31 && this.coinX < 40)
+                        {
+                            this.coinX = 500;
+                        }
+                    }
+                }
+
+                this.points += 10;
+                this.score.Text = this.points.ToString();
+            }
+
+            this.levelValue = this.level / 50;
+            this.currentLevel.Text = this.levelValue.ToString();
+            if (this.lives == 0)
+            {
+                this.gameover.Text = "Game Over!";
+            }
+        }
+    }
+}
---
+++ DriveCarApplication/DriveCarApplication/Form1.designer.cs
@@ -0,0 +1,234 @@
+﻿namespace DriveCarApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.screen = new System.Windows.Forms.Panel();
+            this.Coin = new System.Windows.Forms.PictureBox();
+            this.gameover = new System.Windows.Forms.Label();
+            this.anotherCar2 = new System.Windows.Forms.PictureBox();
+            this.anotherCar = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.score = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.scoreboard = new System.Windows.Forms.Label();
+            this.livesboard = new System.Windows.Forms.Label();
+            this.currentLevel = new System.Windows.Forms.Label();
+            this.levelboard = new System.Windows.Forms.Label();
+            this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.anotherCar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.anotherCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.screen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("screen.BackgroundImage")));
+            this.screen.Controls.Add(this.Coin);
+            this.screen.Controls.Add(this.gameover);
+            this.screen.Controls.Add(this.anotherCar2);
+            this.screen.Controls.Add(this.anotherCar);
+            this.screen.Controls.Add(this.player);
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(267, 458);
+            this.screen.TabIndex = 0;
+            this.screen.Paint += new System.Windows.Forms.PaintEventHandler(this.screen_Paint);
+            // 
+            // Coin
+            // 
+            this.Coin.BackColor = System.Drawing.Color.Transparent;
+            this.Coin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Coin.BackgroundImage")));
+            this.Coin.Location = new System.Drawing.Point(186, 61);
+            this.Coin.Name = "Coin";
+            this.Coin.Size = new System.Drawing.Size(52, 48);
+            this.Coin.TabIndex = 6;
+            this.Coin.TabStop = false;
+            // 
+            // gameover
+            // 
+            this.gameover.AutoSize = true;
+            this.gameover.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gameover.Location = new System.Drawing.Point(54, 213);
+            this.gameover.Name = "gameover";
+            this.gameover.Size = new System.Drawing.Size(0, 31);
+            this.gameover.TabIndex = 5;
+            // 
+            // anotherCar2
+            // 
+            this.anotherCar2.BackColor = System.Drawing.Color.Transparent;
+            this.anotherCar2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("anotherCar2.BackgroundImage")));
+            this.anotherCar2.Location = new System.Drawing.Point(50, 138);
+            this.anotherCar2.Name = "anotherCar2";
+            this.anotherCar2.Size = new System.Drawing.Size(53, 95);
+            this.anotherCar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.anotherCar2.TabIndex = 4;
+            this.anotherCar2.TabStop = false;
+            // 
+            // anotherCar
+            // 
+            this.anotherCar.BackColor = System.Drawing.Color.Transparent;
+            this.anotherCar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("anotherCar.BackgroundImage")));
+            this.anotherCar.Location = new System.Drawing.Point(110, 50);
+            this.anotherCar.Name = "anotherCar";
+            this.anotherCar.Size = new System.Drawing.Size(51, 97);
+            this.anotherCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.anotherCar.TabIndex = 1;
+            this.anotherCar.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("player.BackgroundImage")));
+            this.player.Location = new System.Drawing.Point(50, 350);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(53, 93);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(273, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "3";
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.score.Location = new System.Drawing.Point(273, 53);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(30, 31);
+            this.score.TabIndex = 2;
+            this.score.Text = "0";
+            this.score.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // scoreboard
+            // 
+            this.scoreboard.AutoSize = true;
+            this.scoreboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreboard.Location = new System.Drawing.Point(273, 22);
+            this.scoreboard.Name = "scoreboard";
+            this.scoreboard.Size = new System.Drawing.Size(125, 31);
+            this.scoreboard.TabIndex = 4;
+            this.scoreboard.Text = "SCORE:";
+            // 
+            // livesboard
+            // 
+            this.livesboard.AutoSize = true;
+            this.livesboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.livesboard.Location = new System.Drawing.Point(271, 168);
+            this.livesboard.Name = "livesboard";
+            this.livesboard.Size = new System.Drawing.Size(110, 33);
+            this.livesboard.TabIndex = 5;
+            this.livesboard.Text = "LIVES:";
+            // 
+            // currentLevel
+            // 
+            this.currentLevel.AutoSize = true;
+            this.currentLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.currentLevel.Location = new System.Drawing.Point(273, 350);
+            this.currentLevel.Name = "currentLevel";
+            this.currentLevel.Size = new System.Drawing.Size(86, 31);
+            this.currentLevel.TabIndex = 6;
+            this.currentLevel.Text = " dddd";
+            // 
+            // levelboard
+            // 
+            this.levelboard.AutoSize = true;
+            this.levelboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.levelboard.Location = new System.Drawing.Point(271, 319);
+            this.levelboard.Name = "levelboard";
+            this.levelboard.Size = new System.Drawing.Size(112, 31);
+            this.levelboard.TabIndex = 7;
+            this.levelboard.Text = "LEVEL:";
+            this.levelboard.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 458);
+            this.Controls.Add(this.levelboard);
+            this.Controls.Add(this.currentLevel);
+            this.Controls.Add(this.livesboard);
+            this.Controls.Add(this.scoreboard);
+            this.Controls.Add(this.screen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.score);
+            this.Location = new System.Drawing.Point(55, 55);
+            this.Name = "Form1";
+            this.Text = "GameTest 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.screen.ResumeLayout(false);
+            this.screen.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.anotherCar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.anotherCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox anotherCar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.PictureBox anotherCar2;
+        private System.Windows.Forms.Label scoreboard;
+        private System.Windows.Forms.Label livesboard;
+        private System.Windows.Forms.Label currentLevel;
+        private System.Windows.Forms.Label levelboard;
+        private System.Windows.Forms.Label gameover;
+        private System.Windows.Forms.PictureBox Coin;
+    }
+}
+
